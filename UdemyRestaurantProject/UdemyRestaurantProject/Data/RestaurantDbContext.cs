4f664678479d8f9b704be7f2fcad3727abf8b11a
Tatiana <tatjana.pasechnik@gmail.com>
2022-10-11 20:13:03 +1300

    initial commit

---
+++ UdemyRestaurantProject/UdemyRestaurantProject/Data/RestaurantDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UdemyRestaurantProject.Model;
+
+namespace UdemyRestaurantProject.Data
+{
+    public class RestaurantDbContext :DbContext
+    {
+        public RestaurantDbContext(DbContextOptions<RestaurantDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Category> RestaurantCategory { get; set; }
+    }
+}
